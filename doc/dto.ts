6cdda870533a46b3ed805780fc113faa9df8d694
Syedar <1270261172@qq.com>
2025-02-26 20:24:44 +0800

    初步编写了dto类（doc/dto.ts）

--- doc/dto.ts
+++ doc/dto.ts
@@ -1,20 +1,60 @@
+//
+interface PatientInfo{
+    id:number;
+    name: string;
+    age: number;
+    gender: string;
+}
+
+interface  HistoryCase{
+    id:number;
+    date:Date;
+    description:string;
+}
+
+interface  ImageData{
+    id:number;
+    image: string;
+}
+
+type diseaseRate = { name:string, rate:number };
+interface AiDiag{
+    id:number;
+    diagnosis: string[],
+    suggestions: string[],
+    chartData: diseaseRate[],
+}
+
+type doctorSuggestion = { name: string; position: string;  suggestions: string };
+interface NormalDiag{
+    id:number;
+    date:Date;
+    doctorName:string;
+    department:string;
+    suggestions:doctorSuggestion[];
+}
+
+
+
+// DTO
 // 诊断分析界面（病例界面）
-interface diagnosedCaseDTO{
-    patientInfo: any,
-    historyCases: [any],
-    imageData: [any],
-    aiDiag: any,
-    normalDiag: any,
+interface DiagnosedCaseDTO{
+    patientInfo: PatientInfo,
+    historyCases: HistoryCase[],
+    imageData: ImageData[],
+    aiDiag: AiDiag,
+    normalDiag: NormalDiag,
 }
 
 // 快速创建/导入病例界面
-interface caseDTO{
-    patientInfo: any,
-    imageData: [any],
+interface CaseDTO{
+    patientInfo: PatientInfo,
+    imageData: ImageData[],
 }
 
+type Case = { id:number, patientInfo:PatientInfo, responsibleDoctor:string, createDate:Date, lastUpdateDate:Date};
 // 病例库界面
-interface caseLibDTO{
-    casesList:[any],
+interface CaseLibDTO{
+    casesList:Case[],
 }
 
